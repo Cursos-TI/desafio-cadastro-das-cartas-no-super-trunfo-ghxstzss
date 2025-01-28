efbfd4a7206940f08a23a8d81a2f4386340136db
ghxstzss <felipsss2004@gmail.com>
2025-01-28 10:00:41 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
+int main(){
 
-int main() {
     int codigo da cidade;
     char nome[80];
     double populaçao;
@@ -11,7 +9,9 @@ int main() {
     double pib;
     int pontos turisticos;
 
-    printf("digite o codigo da cidade: \n");
+    printf("Bem Vindo Ao Jogo De Cartas!");
+
+    printf("digite o codigo da cidade de 01 a 010: \n");
     scanf("%d" , &cidade);
 
     print("digite o nome: \n");
