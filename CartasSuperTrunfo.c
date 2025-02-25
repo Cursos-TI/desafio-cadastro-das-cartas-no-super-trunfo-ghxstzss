b4b59ead08ad5a61771fd0009422b52e7148d07d
felipe <felipsss2004@gmail.com>
2025-02-25 14:06:02 +0000

    NÍVEL AVENTUREIRO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,20 +4,29 @@ int main(){
 
     //declaraçao das variaveis
 
-    char Estado1, Estado2;
-    char CodigoCarta1, CodigoCarta2[3];
-    char nomeCidade1, nomeCidade2[100];
-    int populacao1, populacao2;
-    float areakm1, areakm2;
-    float pib1, pib2;
-    int pontosTuristicos1, pontosTuristicos2;
-    float DensidadePopulacional1, DensidadePopulacional2;
-    float PibPerCapita1, PibPerCapita2;
+    char Estado1;
+    char CodigoCarta1[3];
+    char nomeCidade1[100];
+    int populacao1;
+    float areakm1;
+    float pib1;
+    int pontosTuristicos1;
+    float DensidadePopulacional1;
+    float PibPerCapita1;
+    
+    char Estado2;
+    char CodigoCarta2[4];
+    char nomeCidade2[100];
+    int populacao2;
+    float areakm2;
+    float pib2;
+    int pontosTuristicos2;
+    float DensidadePopulacional2;
+    float PibPerCapita2;
     
     //CADASTRO DA PRIMEIRA CARTA
 
-    printf("Bem Vindo Ao Jogo De Cartas! \n");
-
+    printf("Bem Vindo ao Jogo De Cartas!!!: \n");
 
     printf("Digite a Inicial Do Estado e o Código da primeira carta de 01 a 03: \n");
     scanf("%c", &Estado1);
@@ -27,7 +36,7 @@ int main(){
     scanf("%s", &nomeCidade1);
 
     printf("Digite a População: \n");
-    scanf("%f", &populacao1);
+    scanf("%d", &populacao1);
 
     printf("Digite a AreaKM: \n");
     scanf("%f", &areakm1);
@@ -41,22 +50,23 @@ int main(){
     //CALCULO DENSIDADE E PIB PER CAPITa
 
     DensidadePopulacional1 = (float) (populacao1 / areakm1);
-    PibPerCapita1 = (float) (populacao1 / pib1);
+    PibPerCapita1 = (float) (pib1 / populacao1);
  
     printf("Densidade Populacional: %f\n", DensidadePopulacional1);
     printf("Pib Per Capita: %f\n", PibPerCapita1);  
     
+    printf("------------------------------------------------------------\n");                                                                                                      
     //CADASTRO SEGUNDA CARTA
 
-    printf("Digite a Inicial Do Estado e o Código da primeira carta de 01 a 03: \n");
+    printf("Digite a Inicial Do Estado e o Código da Segunda carta de 04 a 08: \n");
     scanf("%c", &Estado2);
-    scanf("%2s", &CodigoCarta2);
+    scanf("%4s", &CodigoCarta2);
 
     printf("Digite o nome da Cidade: \n");
-    scanf("%s", &nomeCidade2);
+    scanf("%s", nomeCidade2);
 
     printf("Digite a População: \n");
-    scanf("%f", &populacao2);
+    scanf("%d", &populacao2);
 
     printf("Digite a AreaKM: \n");
     scanf("%f", &areakm2);
@@ -70,7 +80,7 @@ int main(){
     //CALCULO DENSIDADE E PIB PER CAPITA
 
     DensidadePopulacional2 = (float) (populacao2 / areakm2);
-    PibPerCapita2 = (float) (populacao2 / pib2);
+    PibPerCapita2 = (float) (pib2 / populacao2);
 
     printf("Densidade Populacional: %f\n", DensidadePopulacional2);
     printf("Pib Per Capita: %f\n", PibPerCapita2);
